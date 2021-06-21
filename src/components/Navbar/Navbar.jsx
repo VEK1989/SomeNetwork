6eadef010fd7370ebeb63093e0111484602e7624
Vetal <kononov_vitalik@inbox.ru>
2021-06-21 18:18:37 +0300

    changing a users component to a class component, refactoring

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,9 +7,10 @@ const Navbar = (props) => {
 	return (
 		<nav className={classes.sidebar}>
 			<ul className={classes.list}>
+				<li><NavLink to="/news" activeClassName={classes.active} className={classes.link}>News</NavLink></li>
 				<li><NavLink to="/profile" activeClassName={classes.active} className={classes.link}>Profile</NavLink></li>
 				<li><NavLink to="/messages" activeClassName={classes.active} className={classes.link}>Messages</NavLink></li>
-				<li><NavLink to="/news" activeClassName={classes.active} className={classes.link}>News</NavLink></li>
+				<li><NavLink to="/users" activeClassName={classes.active} className={classes.link}>Users</NavLink></li>
 				<li><NavLink to="/music" activeClassName={classes.active} className={classes.link}>Music</NavLink></li>
 				<li><NavLink to="/settings" activeClassName={classes.active} className={classes.link}>Settings</NavLink></li>
 			</ul>
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,39 +1,45 @@
+import React from 'react';
 import classes from './Users.Module.css'
 import * as axios from 'axios'
 import userPhoto from '../../assets/img/userPhoto.png'
 
-const Users = (props) => {
-	if (props.users.length === 0) {
+class Users extends React.Component {
+	constructor(props) {
+		super(props);
+
 		axios.get('http://localhost:3030/users')
 			.then(response => {
-				props.setUsers(response.data.items)
-			})
+				this.props.setUsers(response.data.items)
+			});
 	}
-	return (
-		<div className={classes.users}>
-			{
-				props.users.map(u => {
-					return <div key={u.id} className={classes.users_item}>
-						<div>
-							<img src={u.photos.small != null ? u.photo.small : userPhoto} className={classes.userPhoto} />
-							<span className={classes.userName}>{u.name}</span>
+
+	render() {
+		return (
+			<div className={classes.users}>
+				{
+					this.props.users.map(u => {
+						return <div key={u.id} className={classes.users_item}>
 							<div>
-								{u.followStatus ? <button className={classes.Btn} onClick={() => { props.unfollow(u.id) }}>Unfollow</button>
-									: <button className={classes.Btn} onClick={() => { props.follow(u.id) }}>Follow</button>}
+								<img src={u.photos.small != null ? u.photo.small : userPhoto} className={classes.userPhoto} />
+								<span className={classes.userName}>{u.name}</span>
+								<div>
+									{u.followStatus ? <button className={classes.Btn} onClick={() => { this.props.unfollow(u.id) }}>Unfollow</button>
+										: <button className={classes.Btn} onClick={() => { this.props.follow(u.id) }}>Follow</button>}
+								</div>
+							</div>
+							<div>
+								<span>{'u.userStatus'}</span>
+							</div>
+							<div>
+								<div>{'u.userCountry'}</div>
+								<div>{'u.userCity'}</div>
 							</div>
 						</div>
-						<div>
-							<span>{'u.userStatus'}</span>
-						</div>
-						<div>
-							<div>{'u.userCountry'}</div>
-							<div>{'u.userCity'}</div>
-						</div>
-					</div>
-				})
-			}
-		</div>
-	);
-};
+					})
+				}
+			</div>
+		);
+	}
+}
 
 export default Users;
